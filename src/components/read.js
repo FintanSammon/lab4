291df15dfe71e5e8820d3dc634d4360bfc6e760f
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-10-16 15:49:22 +0100

    Update read.js

--- src/components/read.js
+++ src/components/read.js
@@ -6,7 +6,7 @@ function Read() {
 
 
     const [data, setData] = useState([]);
-//use effect is a react hook that allows 
+//useEffect is a React Hook that lets you synchronize a component with an external system. 
     useEffect(() => {
         axios.get('https://jsonblob.com/api/jsonblob/1161593332966481920')
             .then(
