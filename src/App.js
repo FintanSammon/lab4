1b43c1be768535fc55f96dc2d004782cb2c3b91f
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-10-22 22:11:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Here are the imports for the necessary styles and components which I implemented into my app.
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
@@ -7,6 +8,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+//// Here I Imported React Router for routing
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Read from './components/read';
 import Create from './components/create';
@@ -14,11 +17,16 @@ import Books from './components/books';
 
 function App() {
   return (
+  //// For this part I set up BrowserRouter for client-side routing
+
     <BrowserRouter>
     <div className="App">
+
+    
+// Here I have a navbar so users can decide which page they would like to view.
+// The pages are Home, Read and Create.     
       
-      
-      <Navbar bg="primary" data-bs-theme="dark">
+     <Navbar bg="primary" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
@@ -29,7 +37,7 @@ function App() {
         </Container>
       </Navbar>
       
-      
+  // Here I defined route handling using React Router     
   <Routes>
   <Route path='/' element={<Content></Content>}></Route>
   <Route path='/read' element={<Read></Read>}></Route>
@@ -46,4 +54,5 @@ function App() {
   );
 }
 
+// Here is the export for the app
 export default App;
